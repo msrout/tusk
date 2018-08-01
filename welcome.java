966e5d6b7cd51ea441b64493633aec54918ba56c
msrout <35970943+msrout@users.noreply.github.com>
2018-08-01 12:12:17 +0200

    added some new concepts

--- welcome.java
+++ welcome.java
@@ -1,11 +1,13 @@
 
-static void Method()
+static void Method(int value)
 {
+	value = value+5;
+	
 	System.out.println("method");
 }
 public static void main(String args[])
 {
 	int i = 5;
-	Method();
+	Method(i);
     System.out.println("Welcome to git hub");
   }
